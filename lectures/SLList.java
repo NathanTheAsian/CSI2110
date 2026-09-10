d910ec44611c9c3d67cd3e9dc630d53f16f81614
Nathan Ng <nathanawesome626@gmail.com>
2026-09-10 19:10:17 -0400

    first push

---
+++ lectures/SLList.java
@@ -0,0 +1,16 @@
+public class SLList {
+    public Node head;
+
+    public SLList(int e) {
+        head = new Node(e, null);
+
+    }
+
+    public void addlast(int e) {
+        Node current = head;
+        while(current.next != null) {
+            
+        }
+
+    }
+}
